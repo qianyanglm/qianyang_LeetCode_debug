7b50f16d4a56391b96397ba9668f0fc777b6998d
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-05-25 17:00:49 +0800

    5.25

---
+++ leetcode/editor/cn/58-length-of-last-word.cpp
@@ -0,0 +1,87 @@
+/**
+<p>Given a string <code>s</code> consisting of words and spaces, return <em>the length of the <strong>last</strong> word in the string.</em></p>
+
+<p>A <strong>word</strong> is a maximal <span data-keyword="substring-nonempty">substring</span> consisting of non-space characters only.</p>
+
+<p>&nbsp;</p> 
+<p><strong class="example">Example 1:</strong></p>
+
+<pre>
+<strong>Input:</strong> s = "Hello World"
+<strong>Output:</strong> 5
+<strong>Explanation:</strong> The last word is "World" with length 5.
+</pre>
+
+<p><strong class="example">Example 2:</strong></p>
+
+<pre>
+<strong>Input:</strong> s = "   fly me   to   the moon  "
+<strong>Output:</strong> 4
+<strong>Explanation:</strong> The last word is "moon" with length 4.
+</pre>
+
+<p><strong class="example">Example 3:</strong></p>
+
+<pre>
+<strong>Input:</strong> s = "luffy is still joyboy"
+<strong>Output:</strong> 6
+<strong>Explanation:</strong> The last word is "joyboy" with length 6.
+</pre>
+
+<p>&nbsp;</p> 
+<p><strong>Constraints:</strong></p>
+
+<ul> 
+ <li><code>1 &lt;= s.length &lt;= 10<sup>4</sup></code></li> 
+ <li><code>s</code> consists of only English letters and spaces <code>' '</code>.</li> 
+ <li>There will be at least one word in <code>s</code>.</li> 
+</ul>
+
+<details><summary><strong>Related Topics</strong></summary>字符串</details><br>
+
+<div>👍 588, 👎 0<span style='float: right;'><span style='color: gray;'><a href='https://github.com/labuladong/fucking-algorithm/discussions/939' target='_blank' style='color: lightgray;text-decoration: underline;'>bug 反馈</a> | <a href='https://labuladong.gitee.io/article/fname.html?fname=jb插件简介' target='_blank' style='color: lightgray;text-decoration: underline;'>使用指南</a> | <a href='https://labuladong.github.io/algo/images/others/%E5%85%A8%E5%AE%B6%E6%A1%B6.jpg' target='_blank' style='color: lightgray;text-decoration: underline;'>更多配套插件</a></span></span></div>
+
+
+
+
+*/
+#include "include/headers.h"
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+//官方
+class Solution
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        int index = s.size() - 1;
+        while (s[index] == ' ')
+        {
+            index--;
+        }
+        int wordLength = 0;
+        while (index >= 0 && s[index] != ' ')
+        {
+            wordLength++;
+            index--;
+        }
+        return wordLength;
+    }
+};
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    vector<vector<int>> data1{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    string res = "Hello LeetCode";
+    cout << res << endl;
+    cout << s.lengthOfLastWord(res) << endl;
+
+
+    return 0;
+}
