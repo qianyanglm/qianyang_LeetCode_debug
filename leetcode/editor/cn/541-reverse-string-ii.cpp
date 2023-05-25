7b7e608cdf55ff56ce681e44f42c0f40b014d056
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-05-25 19:38:30 +0800

    5.25

---
+++ leetcode/editor/cn/541-reverse-string-ii.cpp
@@ -0,0 +1,67 @@
+/**
+<p>Given a string <code>s</code> and an integer <code>k</code>, reverse the first <code>k</code> characters for every <code>2k</code> characters counting from the start of the string.</p>
+
+<p>If there are fewer than <code>k</code> characters left, reverse all of them. If there are less than <code>2k</code> but greater than or equal to <code>k</code> characters, then reverse the first <code>k</code> characters and leave the other as original.</p>
+
+<p>&nbsp;</p> 
+<p><strong class="example">Example 1:</strong></p> 
+<pre><strong>Input:</strong> s = "abcdefg", k = 2
+<strong>Output:</strong> "bacdfeg"
+</pre>
+<p><strong class="example">Example 2:</strong></p> 
+<pre><strong>Input:</strong> s = "abcd", k = 2
+<strong>Output:</strong> "bacd"
+</pre> 
+<p>&nbsp;</p> 
+<p><strong>Constraints:</strong></p>
+
+<ul> 
+ <li><code>1 &lt;= s.length &lt;= 10<sup>4</sup></code></li> 
+ <li><code>s</code> consists of only lowercase English letters.</li> 
+ <li><code>1 &lt;= k &lt;= 10<sup>4</sup></code></li> 
+</ul>
+
+<details><summary><strong>Related Topics</strong></summary>双指针 | 字符串</details><br>
+
+<div>👍 470, 👎 0<span style='float: right;'><span style='color: gray;'><a href='https://github.com/labuladong/fucking-algorithm/discussions/939' target='_blank' style='color: lightgray;text-decoration: underline;'>bug 反馈</a> | <a href='https://labuladong.gitee.io/article/fname.html?fname=jb插件简介' target='_blank' style='color: lightgray;text-decoration: underline;'>使用指南</a> | <a href='https://labuladong.github.io/algo/images/others/%E5%85%A8%E5%AE%B6%E6%A1%B6.jpg' target='_blank' style='color: lightgray;text-decoration: underline;'>更多配套插件</a></span></span></div>
+
+
+
+
+*/
+#include "include/headers.h"
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+//官方
+class Solution
+{
+public:
+    string reverseStr(string s, int k)
+    {
+        int n = s.length();
+        for (int i = 0; i < n; i += 2 * k)
+        {
+            reverse(s.begin() + i, s.begin() + min(i + k, n));
+        }
+        return s;
+    }
+};
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    vector<vector<int>> data1{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    string res = "abcdefg";
+    string res1;
+    cout << res << endl;
+    res1 = s.reverseStr(res, 2);
+    cout << res1 << endl;
+
+
+    return 0;
+}
