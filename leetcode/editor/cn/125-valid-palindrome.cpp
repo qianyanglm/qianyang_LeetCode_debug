208da683b5f6b294dca184bf928c50d6bee00308
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-05-25 15:47:39 +0800

    5.25

---
+++ leetcode/editor/cn/125-valid-palindrome.cpp
@@ -0,0 +1,85 @@
+/**
+<p>A phrase is a <strong>palindrome</strong> if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.</p>
+
+<p>Given a string <code>s</code>, return <code>true</code><em> if it is a <strong>palindrome</strong>, or </em><code>false</code><em> otherwise</em>.</p>
+
+<p>&nbsp;</p> 
+<p><strong class="example">Example 1:</strong></p>
+
+<pre>
+<strong>Input:</strong> s = "A man, a plan, a canal: Panama"
+<strong>Output:</strong> true
+<strong>Explanation:</strong> "amanaplanacanalpanama" is a palindrome.
+</pre>
+
+<p><strong class="example">Example 2:</strong></p>
+
+<pre>
+<strong>Input:</strong> s = "race a car"
+<strong>Output:</strong> false
+<strong>Explanation:</strong> "raceacar" is not a palindrome.
+</pre>
+
+<p><strong class="example">Example 3:</strong></p>
+
+<pre>
+<strong>Input:</strong> s = " "
+<strong>Output:</strong> true
+<strong>Explanation:</strong> s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+</pre>
+
+<p>&nbsp;</p> 
+<p><strong>Constraints:</strong></p>
+
+<ul> 
+ <li><code>1 &lt;= s.length &lt;= 2 * 10<sup>5</sup></code></li> 
+ <li><code>s</code> consists only of printable ASCII characters.</li> 
+</ul>
+
+<details><summary><strong>Related Topics</strong></summary>双指针 | 字符串</details><br>
+
+<div>👍 638, 👎 0<span style='float: right;'><span style='color: gray;'><a href='https://github.com/labuladong/fucking-algorithm/discussions/939' target='_blank' style='color: lightgray;text-decoration: underline;'>bug 反馈</a> | <a href='https://labuladong.gitee.io/article/fname.html?fname=jb插件简介' target='_blank' style='color: lightgray;text-decoration: underline;'>使用指南</a> | <a href='https://labuladong.github.io/algo/images/others/%E5%85%A8%E5%AE%B6%E6%A1%B6.jpg' target='_blank' style='color: lightgray;text-decoration: underline;'>更多配套插件</a></span></span></div>
+
+
+
+
+*/
+#include "include/headers.h"
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+//官方
+class Solution
+{
+public:
+    bool isPalindrome(string s)
+    {
+        string sgood;
+        for (char ch: s)
+        {
+            if (isalnum(ch))
+            {
+                sgood += tolower(ch);
+            }
+        }
+        string sgood_rev(sgood.rbegin(), sgood.rend());
+        return sgood == sgood_rev;
+    }
+};
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    vector<vector<int>> data1{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    auto res = "H";
+    cout << res << endl;
+
+    cout << s.isPalindrome(res) << endl;
+
+    return 0;
+}
