323b35c401a6376f608b4f898e590252e9634843
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-07-09 09:44:15 +0800

    7.9

---
+++ leetcode/editor/cn/107-binary-tree-level-order-traversal-ii.cpp
@@ -0,0 +1,106 @@
+/**
+<p>Given the <code>root</code> of a binary tree, return <em>the bottom-up level order traversal of its nodes' values</em>. (i.e., from left to right, level by level from leaf to root).</p>
+
+<p>&nbsp;</p> 
+<p><strong class="example">Example 1:</strong></p> 
+<img alt="" src="https://assets.leetcode.com/uploads/2021/02/19/tree1.jpg" style="width: 277px; height: 302px;" /> 
+<pre>
+<strong>Input:</strong> root = [3,9,20,null,null,15,7]
+<strong>Output:</strong> [[15,7],[9,20],[3]]
+</pre>
+
+<p><strong class="example">Example 2:</strong></p>
+
+<pre>
+<strong>Input:</strong> root = [1]
+<strong>Output:</strong> [[1]]
+</pre>
+
+<p><strong class="example">Example 3:</strong></p>
+
+<pre>
+<strong>Input:</strong> root = []
+<strong>Output:</strong> []
+</pre>
+
+<p>&nbsp;</p> 
+<p><strong>Constraints:</strong></p>
+
+<ul> 
+ <li>The number of nodes in the tree is in the range <code>[0, 2000]</code>.</li> 
+ <li><code>-1000 &lt;= Node.val &lt;= 1000</code></li> 
+</ul>
+
+<details><summary><strong>Related Topics</strong></summary>树 | 广度优先搜索 | 二叉树</details><br>
+
+<div>👍 693, 👎 0<span style='float: right;'><span style='color: gray;'><a href='https://github.com/labuladong/fucking-algorithm/discussions/939' target='_blank' style='color: lightgray;text-decoration: underline;'>bug 反馈</a> | <a href='https://labuladong.gitee.io/article/fname.html?fname=jb插件简介' target='_blank' style='color: lightgray;text-decoration: underline;'>使用指南</a> | <a href='https://labuladong.github.io/algo/images/others/%E5%85%A8%E5%AE%B6%E6%A1%B6.jpg' target='_blank' style='color: lightgray;text-decoration: underline;'>更多配套插件</a></span></span></div>
+
+
+
+
+*/
+#include "include/headers.h"
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+//二叉树层次遍历即可，最后反转一下数组即可
+class Solution
+{
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode *root)
+    {
+        queue<TreeNode *> que;
+        if (root != NULL)
+        {
+            que.push(root);
+        }
+        vector<vector<int>> result;
+        while (!que.empty())
+        {
+            int size = que.size();
+            vector<int> vec;
+            for (int i = 0; i < size; ++i)
+            {
+                TreeNode *node = que.front();
+                que.pop();
+                vec.push_back(node->val);
+                if (node->left)
+                {
+                    que.push(node->left);
+                }
+                if (node->right)
+                {
+                    que.push(node->right);
+                }
+            }
+            result.push_back(vec);
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    vector<vector<int>> data1{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    auto res = "Hello LeetCode";
+    cout << res << endl;
+
+
+    return 0;
+}
