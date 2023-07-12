c6c3abaf12754c05918481618bf596c1bf30ff72
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-07-12 11:09:35 +0800

    7.12

--- leetcode/editor/cn/257-binary-tree-paths.cpp
+++ leetcode/editor/cn/257-binary-tree-paths.cpp
@@ -48,12 +48,45 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//二叉树第十三
+//二叉树
 class Solution
 {
+private:
+    void traversal(TreeNode *cur, vector<int> &path, vector<string> &result)
+    {
+        path.push_back(cur->val);
+        if (cur->left == NULL && cur->right == NULL)
+        {
+            string sPath;
+            for (int i = 0; i < path.size() - 1; ++i)
+            {
+                sPath += to_string(path[i]);
+                sPath += "->";
+            }
+            sPath += to_string(path[path.size() - 1]);
+            result.push_back(sPath);
+            return;
+        }
+        if (cur->left)
+        {
+            traversal(cur->left, path, result);
+            path.pop_back();
+        }
+        if (cur->right)
+        {
+            traversal(cur->right, path, result);
+            path.pop_back();
+        }
+    }
+
 public:
     vector<string> binaryTreePaths(TreeNode *root)
     {
+        vector<string> result;
+        vector<int> path;
+        if (root == NULL) return result;
+        traversal(root, path, result);
+        return result;
     }
 };
 
