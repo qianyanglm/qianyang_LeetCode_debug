631fd3c0ac36e2d2217dea00a2f27f502fa4d029
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2023-07-28 08:43:47 +0800

    7.28

---
+++ leetcode/editor/cn/45-jump-game-ii.cpp
@@ -0,0 +1,71 @@
+/**
+<p>You are given a <strong>0-indexed</strong> array of integers <code>nums</code> of length <code>n</code>. You are initially positioned at <code>nums[0]</code>.</p>
+
+<p>Each element <code>nums[i]</code> represents the maximum length of a forward jump from index <code>i</code>. In other words, if you are at <code>nums[i]</code>, you can jump to any <code>nums[i + j]</code> where:</p>
+
+<ul> 
+ <li><code>0 &lt;= j &lt;= nums[i]</code> and</li> 
+ <li><code>i + j &lt; n</code></li> 
+</ul>
+
+<p>Return <em>the minimum number of jumps to reach </em><code>nums[n - 1]</code>. The test cases are generated such that you can reach <code>nums[n - 1]</code>.</p>
+
+<p>&nbsp;</p> 
+<p><strong class="example">Example 1:</strong></p>
+
+<pre>
+<strong>Input:</strong> nums = [2,3,1,1,4]
+<strong>Output:</strong> 2
+<strong>Explanation:</strong> The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+</pre>
+
+<p><strong class="example">Example 2:</strong></p>
+
+<pre>
+<strong>Input:</strong> nums = [2,3,0,1,4]
+<strong>Output:</strong> 2
+</pre>
+
+<p>&nbsp;</p> 
+<p><strong>Constraints:</strong></p>
+
+<ul> 
+ <li><code>1 &lt;= nums.length &lt;= 10<sup>4</sup></code></li> 
+ <li><code>0 &lt;= nums[i] &lt;= 1000</code></li> 
+ <li>It's guaranteed that you can reach <code>nums[n - 1]</code>.</li> 
+</ul>
+
+<details><summary><strong>Related Topics</strong></summary>贪心 | 数组 | 动态规划</details><br>
+
+<div>👍 2181, 👎 0<span style='float: right;'><span style='color: gray;'><a href='https://github.com/labuladong/fucking-algorithm/discussions/939' target='_blank' style='color: lightgray;text-decoration: underline;'>bug 反馈</a> | <a href='https://labuladong.gitee.io/article/fname.html?fname=jb插件简介' target='_blank' style='color: lightgray;text-decoration: underline;'>使用指南</a> | <a href='https://labuladong.github.io/algo/images/others/%E5%85%A8%E5%AE%B6%E6%A1%B6.jpg' target='_blank' style='color: lightgray;text-decoration: underline;'>更多配套插件</a></span></span></div>
+
+
+
+
+*/
+#include "include/headers.h"
+
+//leetcode submit region begin(Prohibit modification and deletion)
+//贪心
+class Solution
+{
+public:
+    int jump(vector<int> &nums)
+    {
+    }
+};
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    vector<vector<int>> data1{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    auto res = "Hello LeetCode";
+    cout << res << endl;
+
+
+    return 0;
+}
---
+++ leetcode/editor/cn/55-jump-game.cpp
@@ -0,0 +1,72 @@
+/**
+<p>You are given an integer array <code>nums</code>. You are initially positioned at the array's <strong>first index</strong>, and each element in the array represents your maximum jump length at that position.</p>
+
+<p>Return <code>true</code><em> if you can reach the last index, or </em><code>false</code><em> otherwise</em>.</p>
+
+<p>&nbsp;</p> 
+<p><strong class="example">Example 1:</strong></p>
+
+<pre>
+<strong>Input:</strong> nums = [2,3,1,1,4]
+<strong>Output:</strong> true
+<strong>Explanation:</strong> Jump 1 step from index 0 to 1, then 3 steps to the last index.
+</pre>
+
+<p><strong class="example">Example 2:</strong></p>
+
+<pre>
+<strong>Input:</strong> nums = [3,2,1,0,4]
+<strong>Output:</strong> false
+<strong>Explanation:</strong> You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+</pre>
+
+<p>&nbsp;</p> 
+<p><strong>Constraints:</strong></p>
+
+<ul> 
+ <li><code>1 &lt;= nums.length &lt;= 10<sup>4</sup></code></li> 
+ <li><code>0 &lt;= nums[i] &lt;= 10<sup>5</sup></code></li> 
+</ul>
+
+<details><summary><strong>Related Topics</strong></summary>贪心 | 数组 | 动态规划</details><br>
+
+<div>👍 2396, 👎 0<span style='float: right;'><span style='color: gray;'><a href='https://github.com/labuladong/fucking-algorithm/discussions/939' target='_blank' style='color: lightgray;text-decoration: underline;'>bug 反馈</a> | <a href='https://labuladong.gitee.io/article/fname.html?fname=jb插件简介' target='_blank' style='color: lightgray;text-decoration: underline;'>使用指南</a> | <a href='https://labuladong.github.io/algo/images/others/%E5%85%A8%E5%AE%B6%E6%A1%B6.jpg' target='_blank' style='color: lightgray;text-decoration: underline;'>更多配套插件</a></span></span></div>
+
+
+
+
+*/
+#include "include/headers.h"
+
+//leetcode submit region begin(Prohibit modification and deletion)
+//贪心
+class Solution
+{
+public:
+    bool canJump(vector<int> &nums)
+    {
+        int cover = 0;
+        if (nums.size() == 1) return true;//只有一个元素，就是能达到
+        for (int i = 0; i <= cover; ++i)  //主意这里是小于等于cover
+        {
+            cover = max(i + nums[i], cover);
+            if (cover >= nums.size() - 1) return true;//说明可以覆盖到终点了
+        }
+        return false;
+    }
+};
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+int main()
+{
+    Solution s;
+    vector<int> data{7, 1, 5, 3, 6, 4};
+    vector<vector<int>> data1{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    auto res = "Hello LeetCode";
+    cout << res << endl;
+
+
+    return 0;
+}
