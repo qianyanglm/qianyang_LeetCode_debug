c2382bebb1612e0e9f595e6ec46cf6d3a9971017
qianyanglm <63186194+qianyanglm@users.noreply.github.com>
2024-03-03 19:51:35 +0800

    3.03 我回来了

--- leetcode/editor/cn/704-binary-search.cpp
+++ leetcode/editor/cn/704-binary-search.cpp
@@ -74,22 +74,24 @@ class Solution
 public:
     int search(vector<int> &nums, int target)
     {
+
         int left = 0;
         int right = nums.size() - 1;
         while (left <= right)
         {
-            int middle = (right - left) / 2 + left;
-            if (nums[middle] == target)
-            {
-                return middle;
-            }
-            else if (nums[middle] < target)
+            int middle = (left + right) / 2;
+            //            int middle=left + ((right - left) / 2);
+            if (nums[middle] < target)
             {
                 left = middle + 1;
             }
             else if (nums[middle] > target)
             {
                 right = middle - 1;
+            }
+            else
+            {
+                return middle;
             }
         }
         return -1;
